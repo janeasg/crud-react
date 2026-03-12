9bd1fa3f970c9483df28ae9e9925bd9e288ae584
janeasg <jdasguimaraes@gmail.com>
2026-03-12 20:52:04 -0300

    Formulário inicial do Crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
 function App() {
+  const [Itens, setItens] = useState([]);
+  const [texto, setTexto] = useState("");
+
+  function adicionar() {
+    setItens([...Itens, texto]);
+    setTexto("");
+  }
+
+  function remover(index) {
+    setItens(Itens.filter((_, i) => i !== index));
+  }
+
+
 
   return (
     <>
-      <h1>Jane</h1>
+      <input value={texto} onChange={(e) => setTexto(e.target.value)} />
+      <button onClick={adicionar}>Adicionar</button>
+      <ul>
+        {Itens.map((item, index) => (
+          <li key={index}>
+            {item} <button onClick={() => remover(index)}>Remover</button>
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
+
 
 export default App
